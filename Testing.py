8b4881210fab2d4c326df379839c13db6164a305
wolf-null <57221440+wolf-null@users.noreply.github.com>
2021-10-14 10:27:03 +0300

    pre-merge

--- Testing.py
+++ Testing.py
@@ -17,6 +17,7 @@ import multiprocessing as mlproc
 from lib.Hosts.MasterHost import MasterHost
 from lib.Nodes.HeartbeatNode import HeartbeatNode
 
+"""
 if __name__ == '__main__':
     master = MasterHost(name='master')
     node_Alice = HeartbeatNode(index='Alice', heartbeat_dst=['Bob', 'Charlie'])
@@ -27,41 +28,12 @@ if __name__ == '__main__':
     master.run()
     master.exec()
 
+"""
+"""
+class Abase:
+    def __init__(self):
+        self.awoo = 'awoo'
 
-    '''
-    n = 20
+    def
+"""
 
-    def save_stat(gt, st):
-        st('sum_wealth', gt('sum_wealth', 0) + gt('wealth', 0))
-
-    def color_rule(data):
-        return (data['sum_wealth'] if 'sum_wealth' in data else 0, 0, 0)
-
-    network = SameSegments(BuildGridGraph(node_initializer=lambda: SimpleNodeIndirected(initial_wealth=1000, max_transaction=10, idle_threshold=-1)), n_segments=2, random_order=False).build(n)
-    initial = network.hist_values('wealth')
-    stage_exec = StageExecAsync(network)
-    stage_graplot = StagePlotGraph(network, color_rule=color_rule, bgcolor='black', ground_normalize=False)
-
-    print('start load')
-    iteration = 0
-    while not keyboard.is_pressed('ctrl+break'):
-        network.apply_get_set(save_stat)
-        stage_graplot.run()
-        stage_exec.run()
-        hist = network.hist_values('wealth')
-        iteration += 1
-        print('\riter: {0}'.format(iteration), end='')
-        time.sleep(0.1)
-
-    stage_exec.halt()
-
-    time.sleep(0.5)
-    stage_graplot.run()
-    time.sleep(0.5)
-
-    while keyboard.is_pressed('ctrl+break'):
-        pass
-    print("press [ ] to exit")
-    while not keyboard.is_pressed(' '):
-        time.sleep(0.1)
-    '''
